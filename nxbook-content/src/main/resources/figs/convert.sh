960c389ea5ad75141b96987bc1bd5018d45e485d
Tim O'Brien <tobrien@discursive.com>
2011-03-15 11:08:22 -0500

    Updated Nexus book for 3.0 release, also adding a script to generate the dropshadow images

---
+++ nxbook-content/src/main/resources/figs/convert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Convert Originals to Web Images
+CONVERT_OPTS=" -verbose -bordercolor white -border 10x10 \( +clone -background black -shadow 80x3+5+5 \) -compose DstOver -composite -compose Over ";
+find ./orig -name "*.png" | tee orig_png.tmp | sed -e 's/orig/web/g' | xargs -L1 echo $CONVERT_OPTS > command.tmp
+paste orig_png.tmp command.tmp | xargs -L1 echo "convert " | sed -e 's/(/\\(/g' | sed -e 's/)/\\)/g' > orig_web.tmp
+sh ./orig_web.tmp
+
+# Convert Originals to Print Images
+CONVERT_OPTS=" -resize 250% -density 150 -verbose -bordercolor white -border 10x10 \( +clone -background black -shadow 80x3+5+5 \) -compose DstOver -composite -compose Over ";
+find ./orig -name "*.png" | tee orig_png.tmp | sed -e 's/orig/print/g' | sed -e 's/png/pdf/g' | xargs -L1 echo $CONVERT_OPTS > command.tmp
+paste orig_png.tmp command.tmp | xargs -L1 echo "convert " | sed -e 's/(/\\(/g' | sed -e 's/)/\\)/g' > orig_print.tmp
+sh ./orig_print.tmp
+
+
+
+
