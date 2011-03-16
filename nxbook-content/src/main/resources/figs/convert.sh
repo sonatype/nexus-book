21ebc810721d499360c2ce5e0c843d98ba3671eb
Tim O'Brien <tobrien@discursive.com>
2011-03-16 12:04:08 -0500

    Updating all figures

--- nxbook-content/src/main/resources/figs/convert.sh
+++ nxbook-content/src/main/resources/figs/convert.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 # Convert Originals to Web Images
-CONVERT_OPTS=" -verbose -bordercolor None -border 10x10 \( +clone -background black -shadow 80x3+5+5 \) -compose DstOver -composite -compose Over ";
-find ./orig -name "*.png" | tee orig_png.tmp | sed -e 's/orig/web/g' | xargs -L1 echo $CONVERT_OPTS > command.tmp
-paste orig_png.tmp command.tmp | xargs -L1 echo "convert " | sed -e 's/(/\\(/g' | sed -e 's/)/\\)/g' > orig_web.tmp
-sh ./orig_web.tmp
+find ./orig -name "*.png" | \
+ xargs -n1 \
+   sh -c 'convert $0 -verbose -compress jpeg \
+            -quality 80 -bordercolor None -border 10x10 \
+            \( +clone -background black -shadow 80x3+5+5 \) \
+            -compose DstOver -composite -compose Over \
+            ./web/`basename $0`'
 
 # Convert Originals to Print Images
-CONVERT_OPTS=" -resize 250% -density 150 -verbose -bordercolor None -border 10x10 \( +clone -background black -shadow 80x3+5+5 \) -compose DstOver -composite -compose Over ";
-find ./orig -name "*.png" | tee orig_png.tmp | sed -e 's/orig/print/g' | sed -e 's/png/pdf/g' | xargs -L1 echo $CONVERT_OPTS > command.tmp
-paste orig_png.tmp command.tmp | xargs -L1 echo "convert " | sed -e 's/(/\\(/g' | sed -e 's/)/\\)/g' > orig_print.tmp
-sh ./orig_print.tmp
+find ./orig -name "*.png" | \
+ xargs -n1 \
+   sh -c 'convert $0 -resize 250% -compress jpeg \
+            -quality 50 -density 150 -verbose \
+            -bordercolor None -border 10x10 \
+            \( +clone -background black -shadow 80x3+5+5 \) \
+            -compose DstOver -composite -compose Over \
+            ./print/`basename $0 .png`.pdf'
+
 
 
 
