c6eedbf01a20cc724a782c7733b4ec9f18a02045
Tim O'Brien <tobrien@discursive.com>
2011-03-20 16:12:50 -0500

    Added a call to test for the image conversion script.  With this change images that have not changed between versions do not need to be regenerated.

--- nxbook-content/src/main/resources/figs/convert.sh
+++ nxbook-content/src/main/resources/figs/convert.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
 # Convert Originals to Web Images
-find ./orig -name "*.png" | \
- xargs -n1 \
-   sh -c 'convert $0 -verbose -compress jpeg \
+for IMAGE in `find ./orig -name "*.png"`
+do
+ DEST_IMAGE=./web/`basename $IMAGE`
+ [ $IMAGE -nt $DEST_IMAGE ] &&
+ convert $IMAGE -verbose -compress jpeg \
             -quality 80 -bordercolor None -border 10x10 \
             \( +clone -background black -shadow 80x3+5+5 \) \
             -compose DstOver -composite -compose Over \
-            ./web/`basename $0`'
+            $DEST_IMAGE
+done
 
 # Convert Originals to Print Images
-find ./orig -name "*.png" | \
- xargs -n1 \
-   sh -c 'convert $0 -resize 250% -compress jpeg \
+for IMAGE in `find ./orig -name "*.png"`
+do
+ DEST_IMAGE=./print/`basename $IMAGE .png`.pdf
+ [ $IMAGE -nt $DEST_IMAGE ] &&
+  convert $IMAGE -resize 250% -compress jpeg \
             -quality 50 -density 150 -verbose \
             -bordercolor None -border 10x10 \
             \( +clone -background black -shadow 80x3+5+5 \) \
             -compose DstOver -composite -compose Over \
-            ./print/`basename $0 .png`.pdf'
+            $DEST_IMAGE
+done
 
 exit 0
 
