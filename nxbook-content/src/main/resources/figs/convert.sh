0678300c0f1f8d2cce464fcfbc478530cf668213
Tim O'Brien <tobrien@discursive.com>
2011-03-20 00:39:19 -0500

    Deleted images generated from original screenshots

--- nxbook-content/src/main/resources/figs/convert.sh
+++ nxbook-content/src/main/resources/figs/convert.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
 # Convert Originals to Web Images
+#find ./orig -name "*.png" | \
+# xargs -n1 \
+#   sh -c 'convert $0 -verbose -compress jpeg \
+#            -quality 80 -bordercolor None -border 10x10 \
+#            \( +clone -background black -shadow 80x3+5+5 \) \
+#            -compose DstOver -composite -compose Over \
+#            ./web/`basename $0`'
+
 find ./orig -name "*.png" | \
  xargs -n1 \
-   sh -c 'convert $0 -verbose -compress jpeg \
-            -quality 80 -bordercolor None -border 10x10 \
-            \( +clone -background black -shadow 80x3+5+5 \) \
-            -compose DstOver -composite -compose Over \
-            ./web/`basename $0`'
+   sh -c 'rm ./web/`basename $0`'
 
 # Convert Originals to Print Images
+#find ./orig -name "*.png" | \
+# xargs -n1 \
+#   sh -c 'convert $0 -resize 250% -compress jpeg \
+#            -quality 50 -density 150 -verbose \
+#            -bordercolor None -border 10x10 \
+#            \( +clone -background black -shadow 80x3+5+5 \) \
+#            -compose DstOver -composite -compose Over \
+#            ./print/`basename $0 .png`.pdf'
+
 find ./orig -name "*.png" | \
  xargs -n1 \
-   sh -c 'convert $0 -resize 250% -compress jpeg \
-            -quality 50 -density 150 -verbose \
-            -bordercolor None -border 10x10 \
-            \( +clone -background black -shadow 80x3+5+5 \) \
-            -compose DstOver -composite -compose Over \
-            ./print/`basename $0 .png`.pdf'
+   sh -c 'rm ./print/`basename $0 .png`.pdf'
 
 
 
