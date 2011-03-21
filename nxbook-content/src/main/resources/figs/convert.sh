153d9395a5f59a1fc93c390c058786ab1e2ac890
Tim O'Brien <tobrien@discursive.com>
2011-03-20 21:34:14 -0500

    And we needed another test clause in this bash script to make sure that we're capturing the case where there is no existing file to test the age of.

--- nxbook-content/src/main/resources/figs/convert.sh
+++ nxbook-content/src/main/resources/figs/convert.sh
@@ -1,28 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 # Convert Originals to Web Images
 for IMAGE in `find ./orig -name "*.png"`
 do
  DEST_IMAGE=./web/`basename $IMAGE`
- [ $IMAGE -nt $DEST_IMAGE ] &&
- convert $IMAGE -verbose -compress jpeg \
+ if [[ ! -e $DEST_IMAGE || $IMAGE -nt $DEST_IMAGE ]] 
+ then
+     convert $IMAGE -verbose -compress jpeg \
             -quality 80 -bordercolor None -border 10x10 \
             \( +clone -background black -shadow 80x3+5+5 \) \
             -compose DstOver -composite -compose Over \
             $DEST_IMAGE
+ fi
 done
 
 # Convert Originals to Print Images
 for IMAGE in `find ./orig -name "*.png"`
 do
  DEST_IMAGE=./print/`basename $IMAGE .png`.pdf
- [ $IMAGE -nt $DEST_IMAGE ] &&
-  convert $IMAGE -resize 250% -compress jpeg \
+ if [[ ! -e $DEST_IMAGE || $IMAGE -nt $DEST_IMAGE ]]
+ then
+     convert $IMAGE -resize 250% -compress jpeg \
             -quality 50 -density 150 -verbose \
             -bordercolor None -border 10x10 \
             \( +clone -background black -shadow 80x3+5+5 \) \
             -compose DstOver -composite -compose Over \
             $DEST_IMAGE
+ fi
 done
 
 exit 0
