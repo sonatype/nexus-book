d91fc0ca03f7c9ba38d751d831d43b2f676eed8b
Tim O'Brien <tobrien@discursive.com>
2011-08-07 22:13:53 -0500

    Adding a script to build the book

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p target/images
+mkdir -p target/figs
+
+# Build the Single HTML Page Version
+asciidoc -o target/book-nexus.html book-nexus.doc 
+
+# Build the PDF
+rm -rf target/images
+rm -rf target/figs
+cp -r figs target
+cp -r images target
+a2x -v -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-nexus.doc
