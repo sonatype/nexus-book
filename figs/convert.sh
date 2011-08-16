4be9d8b7823666420e7d3cbd119fc9a196122d84
Tim O'Brien <tobrien@discursive.com>
2011-08-16 17:19:33 -0500

    Updated all of the figures after cropping for display

--- figs/convert.sh
+++ figs/convert.sh
@@ -14,21 +14,6 @@ do
  fi
 done
 
-# Convert Originals to Print Images
-for IMAGE in `find ./orig -name "*.png"`
-do
- DEST_IMAGE=./print/`basename $IMAGE .png`.pdf
- if [[ ! -e $DEST_IMAGE || $IMAGE -nt $DEST_IMAGE ]]
- then
-     convert $IMAGE -resize 250% -compress jpeg \
-            -quality 50 -density 150 -verbose \
-            -bordercolor None -border 10x10 \
-            \( +clone -background black -shadow 80x3+5+5 \) \
-            -compose DstOver -composite -compose Over \
-            $DEST_IMAGE
- fi
-done
-
 exit 0
 
 
