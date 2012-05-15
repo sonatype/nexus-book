46b806d58ed235820de046dc13a24d50a0692055
Tim O'Brien <tobrien@discursive.com>
2012-05-14 21:44:27 -0500

    Updating Look and Feel of Book, adding sharing buttons

--- assemble.sh
+++ assemble.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+rm -rf target/site/reference
+rm -rf target/site/pdf
+
+
 mkdir -p target/site/reference
 mkdir -p target/site/pdf
+
 cp -r target/book-nexus.chunked/* target/site/reference
+mkdir -p target/site/reference/css
+cp -r site/css/book.css target/site/reference/css
+
 cp target/book-nexus.pdf target/site/pdf/nxbook-pdf.pdf
 python template.py
 rsync -e ssh -av target/site/* deployer@www.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/
