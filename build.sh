67b136a8f7fa346e83cc2197e9715fd133b546a1
Manfred Moser <manfred@simpligility.com>
2012-09-27 09:40:33 -0700

    NXBOOK-680, NXBOOK-679 first promo html inserted after working out how to do it, updated build to work nicely with it as well

--- build.sh
+++ build.sh
@@ -13,16 +13,18 @@ asciidoc -o target/book-nexus.html book-nexus.doc
 # Build the PDF
 rm -rf target/images
 rm -rf target/figs
+rm -rf target/promo_*.*
 cp -r figs target
 cp -r images target
+cp -r promos/* target
 
-a2x --no-xmllint -v -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" -P latex.output.revhistory=0 -s ./latex/custom-docbook.sty" -D target book-nexus.doc
+#a2x --no-xmllint -v -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" -P latex.output.revhistory=0 -s ./latex/custom-docbook.sty" -D target book-nexus.doc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
+#gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
 
-mv target/tmp.pdf target/book-nexus.pdf
+#mv target/tmp.pdf target/book-nexus.pdf
 
 # Build the Chunked HTML
 a2x -v -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1" -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-nexus.doc
