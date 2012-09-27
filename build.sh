5e3838ef4048057b8f4abe3649151b84b457337b
Manfred Moser <manfred@simpligility.com>
2012-09-27 12:23:04 -0700

    added pdf generation back in

--- build.sh
+++ build.sh
@@ -18,13 +18,13 @@ cp -r figs target
 cp -r images target
 cp -r promos/* target
 
-#a2x --no-xmllint -v -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" -P latex.output.revhistory=0 -s ./latex/custom-docbook.sty" -D target book-nexus.doc
+a2x --no-xmllint -v -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" -P latex.output.revhistory=0 -s ./latex/custom-docbook.sty" -D target book-nexus.doc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 
-#gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
 
-#mv target/tmp.pdf target/book-nexus.pdf
+mv target/tmp.pdf target/book-nexus.pdf
 
 # Build the Chunked HTML
 a2x -v -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1" -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-nexus.doc
