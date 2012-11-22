aebdad4aa95bfc99149bf8d28e96741e29ae86c0
Manfred Moser <manfred@simpligility.com>
2012-11-22 11:56:15 -0800

    changed deployment script to use new user for running on bamboo

--- assemble.sh
+++ assemble.sh
@@ -13,4 +13,4 @@ cp -r site/css/book.css target/site/reference/css
 
 cp target/book-nexus.pdf target/site/pdf/nxbook-pdf.pdf
 python template.py
-rsync -e ssh -av target/site/* deployer@www.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/
+rsync -e ssh -av target/site/* deployer@marketing01.int.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/
