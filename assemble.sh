3abf62671d341ea14f029690457aa4e978ca95d2
Manfred Moser <manfred@simpligility.com>
2013-03-14 13:33:40 -0700

    also pushing eval guide html to server

--- assemble.sh
+++ assemble.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 rm -rf target/site/reference
+rm -rf target/site/eval
 rm -rf target/site/pdf
 
-
 mkdir -p target/site/reference
+mkdir -p target/site/eval
 mkdir -p target/site/pdf
 
 cp -r target/book-nexus.chunked/* target/site/reference
 mkdir -p target/site/reference/css
 cp -r site/css/book.css target/site/reference/css
+
+cp -r target/sonatype-nexus-eval-guide.chunked/* target/site/eval
+mkdir -p target/site/eval/css
+cp -r site/css/book.css target/site/eval/css
 
 cp target/book-nexus.pdf target/site/pdf/nxbook-pdf.pdf
 cp target/sonatype-nexus-eval-guide.pdf target/site/pdf/sonatype-nexus-eval-guide.pdf
