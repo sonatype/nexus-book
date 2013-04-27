8741a7c4979da413c31d64cc6dc184c55489e6d2
Manfred Moser <manfred@simpligility.com>
2013-04-26 23:45:24 -0700

    deactivating html version of eval guide since it is in the book and the standalone version is not using the html template, will fix up later

--- assemble.sh
+++ assemble.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf target/site/reference
-rm -rf target/site/eval
+#rm -rf target/site/eval
 rm -rf target/site/pdf
 
 mkdir -p target/site/reference
-mkdir -p target/site/eval
+#mkdir -p target/site/eval
 mkdir -p target/site/pdf
 
 cp -r target/book-nexus.chunked/* target/site/reference
@@ -14,11 +14,11 @@ cp -r site/css target/site/reference
 cp -r site/assets target/site/reference
 cp -r site/images target/site/reference
 
-cp -r target/sonatype-nexus-eval-guide.chunked/* target/site/eval
-mkdir -p target/site/eval/css
-cp -r site/css target/site/eval
-cp -r site/assets target/site/eval
-cp -r site/images target/site/eval
+#cp -r target/sonatype-nexus-eval-guide.chunked/* target/site/eval
+#mkdir -p target/site/eval/css
+#cp -r site/css target/site/eval
+#cp -r site/assets target/site/eval
+#cp -r site/images target/site/eval
 
 cp target/book-nexus.pdf target/site/pdf/nxbook-pdf.pdf
 cp target/sonatype-nexus-eval-guide.pdf target/site/pdf/sonatype-nexus-eval-guide.pdf
