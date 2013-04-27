ee91da69a05441927306cf75c06d27befad875a6
Manfred Moser <manfred@simpligility.com>
2013-04-26 22:57:46 -0700

    fixed up html templated and cleaned up so it mostly works now

--- assemble.sh
+++ assemble.sh
@@ -10,11 +10,15 @@ mkdir -p target/site/pdf
 
 cp -r target/book-nexus.chunked/* target/site/reference
 mkdir -p target/site/reference/css
-cp -r site/css/book.css target/site/reference/css
+cp -r site/css target/site/reference
+cp -r site/assets target/site/reference
+cp -r site/images target/site/reference
 
 cp -r target/sonatype-nexus-eval-guide.chunked/* target/site/eval
 mkdir -p target/site/eval/css
-cp -r site/css/book.css target/site/eval/css
+cp -r site/css target/site/eval
+cp -r site/assets target/site/eval
+cp -r site/images target/site/eval
 
 cp target/book-nexus.pdf target/site/pdf/nxbook-pdf.pdf
 cp target/sonatype-nexus-eval-guide.pdf target/site/pdf/sonatype-nexus-eval-guide.pdf
