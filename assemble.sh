3fee75e4759b51dd0bd30c28cb10cb8f547c7d0b
Manfred Moser <manfred@simpligility.com>
2013-04-30 12:40:01 -0700

    new supposedly working template and adapting of assemble script to include js

--- assemble.sh
+++ assemble.sh
@@ -11,12 +11,14 @@ mkdir -p target/site/pdf
 cp -r target/book-nexus.chunked/* target/site/reference
 mkdir -p target/site/reference/css
 cp -r site/css target/site/reference
+cp -r site/js target/site/reference
 cp -r site/assets target/site/reference
 cp -r site/images target/site/reference
 
 #cp -r target/sonatype-nexus-eval-guide.chunked/* target/site/eval
 #mkdir -p target/site/eval/css
 #cp -r site/css target/site/eval
+#cp -r site/js target/site/eval
 #cp -r site/assets target/site/eval
 #cp -r site/images target/site/eval
 
