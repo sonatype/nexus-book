c50e062c4bec02a04f8ab5952e00ca00dbcefb2f
Manfred Moser <manfred@simpligility.com>
2013-09-19 14:57:24 -0700

    fixed rsync path to ensure --delete works as discovered in the CLM book

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./assemble.sh
 
-rsync -e ssh --delete -av target/site/* deployer@marketing01.int.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/
+rsync -e ssh --delete -av target/site/ deployer@marketing01.int.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/
