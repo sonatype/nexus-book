2fda0b23280a83743b27c81a59705c59fabd0eb4
Manfred Moser <manfred@simpligility.com>
2013-10-16 13:56:53 -0700

    reactivated xmllint

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -u
 ./prepare.sh
 
 echo "Building pdf version of book"
-a2x --no-xmllint -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" --param=doc.lot.show=figure,table -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
+a2x -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" --param=doc.lot.show=figure,table -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 
@@ -26,7 +26,7 @@ echo "done"
 
 echo "Building pdf version of eval guide"
 # Build eval guide
-a2x --no-xmllint -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" --param=doc.lot.show=figure,table -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target sonatype-nexus-eval-guide.asciidoc
+a2x  -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" --param=doc.lot.show=figure,table -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target sonatype-nexus-eval-guide.asciidoc
 echo "done"
 
 echo "Building multi page html version of eval guide"
