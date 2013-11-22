5c1a668e1af4e4a84a665a8d7285bba777baed09
Manfred Moser <manfred@simpligility.com>
2013-11-22 11:46:36 -0800

    helper script for css file

---
+++ copy-template-to-books.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+cp -v site/css/* ../sonatype-clm-book/site/css
+cp -v site/css/* ../maven-example-en/site/css
+cp -v site/css/* ../maven-reference-en/site/css
+cp -v site/css/* ../m2eclipse-book/m2ebook-site/src/site/resources/reference/book-m2e/css
+cp -v site/css/* ../maven-cookbook/mcookbook-site/src/site/resources/reference/book-mcook/css
