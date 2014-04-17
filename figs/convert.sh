a7761a8fc38128354c657750c105ee046a1bc5cd
Manfred Moser <manfred@simpligility.com>
2014-04-17 16:05:42 -0700

    making a flat grey border around images instead of the pseudo 3d drop shadow, also ensures that image never bleeds into the page

--- figs/convert.sh
+++ figs/convert.sh
@@ -6,11 +6,13 @@ do
  DEST_IMAGE=./web/`basename $IMAGE`
  if [[ ! -e $DEST_IMAGE || $IMAGE -nt $DEST_IMAGE ]] 
  then
-     convert $IMAGE -compress jpeg \
-            -quality 80 -bordercolor None -border 3x3 \
-            \( +clone -background black -shadow 80x1+1+1 \) \
-            -compose DstOver -composite -compose Over \
-            $DEST_IMAGE
+# Old way of converting making a drop shadow
+#     convert $IMAGE -compress jpeg \
+#            -quality 80 -bordercolor None -border 3x3 \
+#            \( +clone -background black -shadow 80x1+1+1 \) \
+#            -compose DstOver -composite -compose Over \
+#            $DEST_IMAGE
+      convert $IMAGE -border 5 $DEST_IMAGE
       echo "Converted $IMAGE creating $DEST_IMAGE"
  fi
 done
