656bc54fa0018556792c40442b6bb4942bcb5909
Manfred Moser <manfred@simpligility.com>
2014-05-09 14:53:49 -0700

    publishing to version specific directory as an option

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+
+# fail if anything errors
+set -e
+# fail if a function call is missing an argument
+set -u
+
+# load properties to be able to use them in here
+source nexus-book.properties
+
+echo "nexus_version set to $nexus_version"
+
+# can we get rid of this? 
 ./assemble.sh
 
-rsync -e ssh --delete -av target/site/ deployer@marketing01.int.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/
+function rsyncToProduction {
+    source=$1
+    target=$2
+    options=$3
+    echo "Uploading $1 to $2" 
+    rsync -e ssh $options -av target/$source/ deployer@marketing01.int.sonatype.com:/var/www/domains/sonatype.com/www/shared/books/nexus-book/$target
+}
+
+if [ $publish_master == "true" ]; then
+    rsyncToStage site/reference/ reference --delete
+    rsyncToStage site/pdf/ pdf --delete
+fi
+
+rsyncToStage site/$nexus_version/reference/ $nexus_version/reference --delete
+rsyncToStage site/$nexus_version/pdf/ $nexus_version/pdf --delete
+
+# Important to use separate rsync run WITHOUT --delete since its an archive! and we do NOT want old archives to be deleted
+#rsyncToStage archive/ archive
+
+
+
