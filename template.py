3249d84eb6a16f57340dca52cbcf600c9b0affad
Manfred Moser <manfred@simpligility.com>
2014-05-19 13:33:31 -0700

    avoid templating search page

--- template.py
+++ template.py
@@ -14,14 +14,17 @@ bookId = "ss-book-nxbook"
 
 t = airspeed.Template(open("site/book-template.html", "r").read())
 for infile in glob.glob( os.path.join(path, '*.html') ):
-  print "Reading File: " + infile
-  body = open(infile, "r").read()
-  title = body[ body.index( "<title>" ) + 7 : body.rindex("</title>") ]
-  body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
+  if infile.endswith( 'search.html'):
+    print( "  Ignoring search.html" )
+  else:
+    print "Reading File: " + infile
+    body = open(infile, "r").read()
+    title = body[ body.index( "<title>" ) + 7 : body.rindex("</title>") ]
+    body = body[ body.index( "<body>") + 6 : body.rindex("</body>") ]
 
-  if "index.html" in infile:
-    print ("Replacing bookTitle  - replacing with ToC" )
-    title = "Table of Contents"
-    body = body.replace(bookTitle, title)
+    if "index.html" in infile:
+      print ("Replacing bookTitle  - replacing with ToC" )
+      title = "Table of Contents"
+      body = body.replace(bookTitle, title)
 
-  open(infile, "w").write( t.merge(locals()) );
+    open(infile, "w").write( t.merge(locals()) );
