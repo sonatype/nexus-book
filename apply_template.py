f0d7dee798afdc60bedc3ebb7a5bb09ed02d4003
Manfred Moser <manfred@simpligility.com>
2014-05-21 11:24:07 -0700

    removing unused python script

--- apply_template.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-book_title = "Repository Management with Nexus"
-page_title = "BLAHBLAH"
-page_body = "WHOOP WHOOP"
-template = open('site/book-template.html').read()
-print template
-
