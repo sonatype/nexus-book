24a9575804dc1a9a481fc29563473f3818f51fae
Manfred Moser <manfred@simpligility.com>
2014-09-01 14:36:18 -0700

    removed list of tables and figures from the ToC in pdf document, changed depth of ToC to be less (1)

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -u
 ./prepare.sh
 
 echo "Building pdf version of book"
-a2x -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" --param=doc.lot.show=figure,table -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
+a2x -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="--param toc.section.depth=1 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 
