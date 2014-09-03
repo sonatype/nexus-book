eaccd3d475d411c057489a0f91b2462517ca2d39
Manfred Moser <manfred@simpligility.com>
2014-09-03 10:15:42 -0700

    changed to toc level 0 for pdf docs

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -u
 ./prepare.sh
 
 echo "Building pdf version of book"
-a2x -k -v -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=1 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
+a2x -k -v -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=0 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
@@ -24,7 +24,7 @@ echo "done"
 
 echo "Building pdf version of eval guide"
 # Build eval guide
-a2x  -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target sonatype-nexus-eval-guide.asciidoc
+a2x  -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=0 -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target sonatype-nexus-eval-guide.asciidoc
 echo "done"
 
 echo "Building multi page html version of eval guide"
