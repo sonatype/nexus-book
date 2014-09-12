d7e93697ad294bf9b725a8481b22d6e56876f56b
Manfred Moser <manfred@simpligility.com>
2014-09-12 15:11:44 -0700

    fix bug so that search is added to version specific deployment

--- assemble.sh
+++ assemble.sh
@@ -50,7 +50,7 @@ cp -r site/css target/site/$nexus_version/reference
 cp -r site/js target/site/$nexus_version/reference
 cp -r site/assets target/site/$nexus_version/reference
 cp -r site/images target/site/$nexus_version/reference
-cp site/search.html target/site/reference
+cp site/search.html target/site/$nexus_version/reference
 cp target/book-nexus.pdf target/site/$nexus_version/pdf/nxbook-pdf.pdf
 cp target/sonatype-nexus-eval-guide.pdf target/site/$nexus_version/pdf/sonatype-nexus-eval-guide.pdf
 cp target/book-nexus.epub target/site/$nexus_version/other/nexus-book.epub
