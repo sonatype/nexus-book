31a8b878cc9ddeca55c2fdcc2db154b468da0358
Manfred Moser <manfred@simpligility.com>
2014-09-28 15:18:16 -0700

    activated configuration chapter and started with content adaption

--- build.sh
+++ build.sh
@@ -7,26 +7,27 @@ set -u
 ./prepare.sh
 
 echo "Building pdf version of book"
-a2x -k -v -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=1 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
+a2x -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=1 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
 #mv target/tmp.pdf target/book-nexus.pdf
+echo "...done"
 
 echo "Building epub version of book"
 a2x -fepub -D target book-nexus.asciidoc
-echo "done"
+echo "...done"
 
 echo "Building multi page html version of book"
 # Build the Chunked HTML
 a2x -k -fchunked --icons --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 1" -dbook --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0" -D target book-nexus.asciidoc
-echo "done"
+echo "...done"
 
 echo "Building pdf version of eval guide"
 # Build eval guide
 a2x  -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=1 -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target sonatype-nexus-eval-guide.asciidoc
-echo "done"
+echo "...done"
 
 echo "Building multi page html version of eval guide"
 a2x  -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 1" -dbook --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0" -D target sonatype-nexus-eval-guide.asciidoc
-echo "done"
+echo "...done"
