d655bd4f47859dc4b0b7bb516cf9b940c4f23cab
Manfred Moser <manfred@simpligility.com>
2014-12-22 16:19:26 -0800

    removed assets directory processing since it no longer exists

--- assemble.sh
+++ assemble.sh
@@ -34,7 +34,6 @@ if [ $publish_master == "true" ]; then
     mkdir -p target/site/reference/css
     cp -r site/css target/site/reference
     cp -r site/js target/site/reference
-    cp -r site/assets target/site/reference
     cp -r site/images target/site/reference
     cp site/search.html target/site/reference
     cp target/book-nexus.pdf target/site/pdf/nxbook-pdf.pdf
@@ -48,7 +47,6 @@ cp -r target/book-nexus.chunked/* target/site/$nexus_version/reference
 mkdir -p target/site/$nexus_version/reference/css
 cp -r site/css target/site/$nexus_version/reference
 cp -r site/js target/site/$nexus_version/reference
-cp -r site/assets target/site/$nexus_version/reference
 cp -r site/images target/site/$nexus_version/reference
 cp site/search.html target/site/$nexus_version/reference
 cp target/book-nexus.pdf target/site/$nexus_version/pdf/nxbook-pdf.pdf
