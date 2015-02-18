6164c4c9a37ff0714739398fc4c918bc1d2384d0
Manfred Moser <manfred@simpligility.com>
2015-02-18 15:36:21 -0800

    NXBOOK-787 added attribute for promo and check for it for inclusion to avoid ads in epub and related build breakage

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -u
 ./prepare.sh
 
 echo "Building pdf version of book"
-a2x -k -v -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=0 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
+a2x -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.section.depth=0 -P latex.output.revhistory=0 -P doc.publisher.show=0 -s ./latex/custom-docbook.sty" -D target book-nexus.asciidoc
 
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf pdf/nexus-book-cover.pdf pdf/title-page.pdf pdf/blank-page.pdf target/book-nexus.pdf
 #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=target/tmp.pdf target/book-nexus.pdf
@@ -19,7 +19,7 @@ echo "done"
 
 echo "Building multi page html version of book"
 # Build the Chunked HTML
-a2x -k -fchunked --icons --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 1" -dbook --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0" -D target book-nexus.asciidoc
+a2x -k -a promo -fchunked --icons --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 1" -dbook --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0" -D target book-nexus.asciidoc
 echo "done"
 
 echo "Building pdf version of eval guide"
@@ -28,5 +28,5 @@ a2x  -k -fpdf -dbook --xsl-file=docbook-xsl/fo.xsl --dblatex-opts="-P toc.sectio
 echo "done"
 
 echo "Building multi page html version of eval guide"
-a2x  -k -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 1" -dbook --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0" -D target sonatype-nexus-eval-guide.asciidoc
+a2x  -k -a promo -fchunked --xsl-file=docbook-xsl/custom-chunked.xsl --xsltproc-opts "--stringparam chunk.section.depth 1 --stringparam toc.section.depth 1" -dbook --dblatex-opts=" -P doc.lot.show=  -P latex.output.revhistory=0 -P doc.publisher.show=0" -D target sonatype-nexus-eval-guide.asciidoc
 echo "done"
