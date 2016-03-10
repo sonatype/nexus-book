bd6018feef3aaa571db9aa63148955b6a941e692
Manfred Moser <manfred@simpligility.com>
2016-03-10 09:37:11 -0800

    Update to use separate HTML template project for look and feel
    See https://github.com/sonatype/nexus-book/pull/142
    And https://issues.sonatype.org/browse/NXBOOK-913
    And adapted to 2.11 branch setup
    
    Squashed commit of the following:
    
    commit 7c1af1310ae7306abdd6aae081f68b701caab910
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Thu Mar 10 09:22:35 2016 -0800
    
        - added usage of env variable for local deployment path
        - synced with iq server assemble script in terms of usage of variables for templateScript and docProperties
    
    commit bffb13ef9ac099286b509adcebb52ab15b917ca0
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Thu Mar 3 11:14:57 2016 -0800
    
        removed 2.13 links since its not happening yet
    
    commit 903862b496ba9ca5ef24c8d771f0d379e072ce37
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Wed Mar 2 16:56:39 2016 -0800
    
        added property for search URL
    
    commit b3723f04c8f67e8546c8e654de2fa696e7b52cf0
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Wed Mar 2 11:15:08 2016 -0800
    
        fixed relative depth of links from index page
    
    commit f17049978cb8f58b94b91e5b0fdc251b7715cbad
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Tue Mar 1 14:38:58 2016 -0800
    
        fixed include for properties
    
    commit d462198a2e7a451df8254476333c464ee626ba0a
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Tue Mar 1 12:44:15 2016 -0800
    
        rebranded index page and added links for 2.12 and 2.13
    
    commit 49b41dfac81f28c22ea80be3587d330f7e4b40f4
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Tue Mar 1 10:22:38 2016 -0800
    
        added index publishing
    
    commit 75941bbf0448cd2733c65a22dfc446c96981e0e4
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Tue Mar 1 10:01:53 2016 -0800
    
        script to deploy on local machine emulating production deployment
    
    commit 4928a9522abf2e50b8079b5a43e01fca46a6e8d6
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Mon Feb 29 16:18:27 2016 -0800
    
        usage of properties file with more properties and simplified prop names
    
    commit 3851221f987cd1b6fab1bd975cab8c17566b9729
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Mon Feb 29 12:35:01 2016 -0800
    
        updated assemble script to use wrapper, nealry complete, NXBOOK-913
    
    commit 740a4e7cd567831408599b93d2313c24723fe4f3
    Author: Manfred Moser <manfred@simpligility.com>
    Date:   Mon Feb 29 12:20:26 2016 -0800
    
        removed site stuff that is now in shared repo, NXBOOK-913

--- deploy-locally.sh
+++ deploy-locally.sh
@@ -5,28 +5,28 @@ set -e
 # fail if a function call is missing an argument
 set -u
 
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# last folder needs to be nexus-book to reflect production setup
+dest=$NEXUS_DOCUMENTATION/nexus-book
+mkdir -p $dest
+
 # load properties to be able to use them in here
 source nexus-book.properties
-
 echo "version set to $version"
 
-# can we get rid of this? 
-./assemble.sh
-
-function rsyncToStage {
+function rsyncToDest {
     source=$1
-    target=/var/www/domains/sonatype.com/www/shared/books/nexus-book/$2
+    target=$dest/$2
     options=$3
-    connection=deployer@marketing02.int.sonatype.com
-    echo "Uploading $1 to $2 on $connection"
-    ssh $connection mkdir -pv $target
-    rsync -e ssh $options -av target/$source/ $connection:$target
+    echo "Syncing $1 to $2 "
+    mkdir -p $target
+    rsync $options -av target/$source $target
 }
 
 if [ $publish_master == "true" ]; then
-    rsyncToStage site/reference/ reference --delete
-    rsyncToStage site/pdf/ pdf --delete
-    rsyncToStage site/other/ other --delete
+    rsyncToDest site/reference/ reference --delete
+    rsyncToDest site/pdf/ pdf --delete
+    rsyncToDest site/other/ other --delete
 fi
 
 rsyncToDest site/$version/reference/ $version/reference --delete
@@ -45,6 +45,5 @@ if [ $publish_index == "true" ]; then
     rsyncToDest site/sitemap.xml "" --delete
 fi
 
-# Important to use separate rsync run WITHOUT --delete since its an archive! and we do NOT want old archives to be deleted
-#rsyncToStage archive/ archive
+
 
