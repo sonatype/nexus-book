b379ca85dc7c86ad484e7eb20e3659167702a3e0
Manfred Moser <manfred@simpligility.com>
2016-06-09 14:19:49 -0700

    added templating and deployment of global index page

--- deploy-locally.sh
+++ deploy-locally.sh
@@ -41,6 +41,14 @@ if [ $publish_index == "true" ]; then
     rsyncToDest site/css/ css --delete
     rsyncToDest site/fonts/ fonts --delete
     rsyncToDest site/sitemap.xml "" --delete
+
+
+    rsyncToDest site/nexus-documentation/index.html  "../" --delete
+    rsyncToDest site/js/ "../js" --delete
+    rsyncToDest site/images/ "../images" --delete
+    rsyncToDest site/css/ "../css" --delete
+    rsyncToDest site/fonts/ "../fonts" --delete
+
 fi
 
 
